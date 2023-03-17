5cadff15a9468675b9684aafc188931e49a003ff
thuynt29 <ntt2992003@gmail.com>
2023-03-17 10:03:50 +0700

    first commit

---
+++ bai2.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var expressHbs = require('express-handlebars');
+var app = express();
+app.listen(8000);
+app.engine('.hbs', expressHbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+app.set('view engine','.hbs');
+
+app.get('/home', function(req,res){
+    res.render('index');
+});
